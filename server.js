e196559f015e2913bccb1dec13efee7eb3c3d79b
Harry Tsang <harrytsang0@gmail.com>
2022-04-09 01:52:41 +0100

    Added index html Added user inputs Added submit button No functionality on submit button yet

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 'use strict';
-var http = require('http');
 var port = process.env.PORT || 1337;
+var http = require('http');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+function onRequest(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File Not Found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
+
+http.createServer(onRequest).listen(1337)
