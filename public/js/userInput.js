192943828605243644ae06bbeab869e019e842d8
Harry Tsang <harrytsang0@gmail.com>
2022-04-12 21:48:50 +0100

    Separated resultsList into their own variables

--- public/js/userInput.js
+++ public/js/userInput.js
@@ -1,21 +1,30 @@
 module.exports = { getResults };
 
 function getResults() {
-    const resultsList = document.getElementById('results');
-    let budget = document.getElementById('budget');
+    //let resultsList = document.getElementById('results');
+    let resultsList = "";
+    //let budget = document.getElementById('budget');
 
     new URLSearchParams(window.location.search).forEach((value,
         name) => {
-        resultsList.append(`${name}:${value}, `)
+        //resultsList.append(`${name}:${value}, `)
+        resultsList = (resultsList + name +":"+value+",")
     })
+    console.log(resultsList)
+    let words = resultsList.split(',');
 
-    let words = resultsList.split(', ');
-
-    budget = words[0].substring(8)
-    let resolution = words[1].substring(12)
-    let fps = words[2].substring(5)
-    let performance = words[3].substring(13)
-    let cpuChoice = words[4].substring(11)
-    let gpuChoice = words[5].substring(11)
-    let overclock = words[6].substring(11)
+    let budget = words[0].substring(7)
+    console.log(budget)
+    let resolution = words[1].substring(11)
+    console.log(resolution)
+    let fps = words[2].substring(4)
+    console.log(fps)
+    let performance = words[3].substring(12)
+    console.log(performance)
+    let cpuChoice = words[4].substring(10)
+    console.log(cpuChoice)
+    let gpuChoice = words[5].substring(10)
+    console.log(gpuChoice)
+    let overclock = words[6].substring(10)
+    console.log(overclock)
 }
