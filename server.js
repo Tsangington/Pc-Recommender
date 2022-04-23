64618256453602e74a90f6c478ea014cb894da27
Harry Tsang <harrytsang0@gmail.com>
2022-04-23 17:46:39 +0100

    Added heroku to host the node.js site

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const path = require('path')
 const express = require('express')
 const { GeforceGpuScrape, RadeonGpuScrape, RyzenCpuScrape, IntelCpuScrape, RyzenMoboScrape, IntelMoboScrape } = require('./public/js/webScraper.js')
 const app = express()
-const port = 3000
+const port = 5000;
+const puppeteer = require('puppeteer');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 // Static Files
 app.use(express.static('public'))
@@ -19,6 +20,24 @@ app.set('views', './views')
 app.get('', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
+
+// Support for pkg
+const executablePath =
+    process.env.PUPPETEER_EXECUTABLE_PATH ||
+    (process.pkg
+        ? path.join(
+            path.dirname(process.execPath),
+            'puppeteer',
+            ...puppeteer
+                .executablePath()
+                .split(path.sep)
+                .slice(6), // /snapshot/project/node_modules/puppeteer/.local-chromium
+        )
+        : puppeteer.executablePath());
+
+const browser = puppeteer.launch({
+    executablePath,
+});
 
 new GeforceGpuScrape();
 //new RyzenCpuScrape();
